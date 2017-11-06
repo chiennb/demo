d383d743aca0af05e742950407541c073327cafd
chiennb <chiennb@gmail.com>
2017-11-06 21:40:30 +0700

    Apply login page

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 import { mainRoutes } from './main.routes';
+import { HomeModule } from './home/home.module';
 import { UserModule } from './user/user.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    HomeModule,
     UserModule,
     RouterModule.forChild(mainRoutes)
   ],
